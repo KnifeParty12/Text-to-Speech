3a3be2666bd37dc9c8418c0eb8f1b5e9c622bd54
KnifeParty12 <flashfunk01@gmail.com>
2019-03-25 22:03:37 +0530

    Completed

--- dist/Js/main.js
+++ dist/Js/main.js
@@ -1,7 +1,7 @@
-const synth = window.speechSynthesis; //Init SpeechSynth API
+// Init SpeechSynth API
+const synth = window.speechSynthesis;
 
-//DOM Elements
-
+// DOM Elements
 const textForm = document.querySelector('form');
 const textInput = document.querySelector('#text-input');
 const voiceSelect = document.querySelector('#voice-select');
@@ -11,58 +11,55 @@ const pitch = document.querySelector('#pitch');
 const pitchValue = document.querySelector('#pitch-value');
 const body = document.querySelector('body');
 
-//Browser identifier
-//Firefox 1.0+
-var isFirefox = typeof InstallTrigger !=='undefined';
-
-//Chrom 1+
+var isFirefox = typeof InstallTrigger !== 'undefined';
 
 var isChrome = !!window.chrome && !!window.chrome.webstore;
 
+// Init voices array
 let voices = [];
 
-const getVoioces = () => {
+const getVoices = () => {
     voices = synth.getVoices();
 
-    //Loop through voices and create an option for each one
+    // Loop through voices and create an option for each one
     voices.forEach(voice => {
-        //Create option element
+
+        // Create option element
+
         const option = document.createElement('option');
-        //Fill option with voices and language
+        // Fill option with voice and language
+
         option.textContent = voice.name + '(' + voice.lang + ')';
 
+        // Set needed option attributes
         option.setAttribute('data-lang', voice.lang);
         option.setAttribute('data-name', voice.name);
         voiceSelect.appendChild(option);
     });
 };
 
-if (isFirefox){
-    getVoices();
+getVoices();
+if (synth.onvoiceschanged !== undefined) {
+    synth.onvoiceschanged = getVoices;
 }
-
-if(isChrome){
-    if(synth.onvoiceschanged !== undefined){
-        synth.onvoiceschanged = getVoices;
-    }
-}
-
 
 const speak = () => {
-    // Check if speaking
-    if(synth.speaking) {
+
+    if (synth.speaking) {
         console.error('Already speaking...');
         return;
     }
-    if(textInput.value !== ''){
+    if (textInput.value !== '') {
+
         body.style.background = '#141414 url(img/wave.gif)';
         body.style.backgroundRepeat = 'repeat-x';
         body.style.backgroundSize = '100% 100%';
 
+
         const speakText = new SpeechSynthesisUtterance(textInput.value);
 
         speakText.onend = e => {
-            console.log('Done Speaking....');
+            console.log('Done speaking...');
             body.style.background = '#141414';
         };
 
@@ -74,23 +71,20 @@ const speak = () => {
             'data-name'
         );
 
-        voice.forEach(voice => {
-            if(voice.name === selectedVoice){
+        voices.forEach(voice => {
+            if (voice.name === selectedVoice) {
                 speakText.voice = voice;
             }
         });
 
         speakText.rate = rate.value;
         speakText.pitch = pitch.value;
-
         synth.speak(speakText);
     }
 };
 
-//EVENT LISTENERS
 
-//Text form submit
-textForm.addEventListener('submit', e =>{
+textForm.addEventListener('submit', e => {
     e.preventDefault();
     speak();
     textInput.blur();
@@ -98,6 +92,6 @@ textForm.addEventListener('submit', e =>{
 
 rate.addEventListener('change', e => (rateValue.textContent = rate.value));
 
-pitch.addEventListener('change', e =>(pitchValue.textContent = pitch.value));
+pitch.addEventListener('change', e => (pitchValue.textContent = pitch.value));
 
 voiceSelect.addEventListener('change', e => speak());
